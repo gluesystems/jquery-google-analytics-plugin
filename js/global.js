d487408934877dc78794a54c740650cf8ce4b78f
gluesystems <systems@glueisobar.com>
2013-06-24 17:37:33 +0100

    Added commented out sections showing example usage

--- js/global.js
+++ js/global.js
@@ -1,7 +1,23 @@
 var jQueryPluginTracking = function() {
 	function init() {
 		$(document).gaTracking({
-			gaAccountID : 'UA-1352431-7' // ensure you change this to your Google Analytics site tracking ID. This is in JC.
+			// ensure you change this to your Google Analytics site tracking ID. This is in JC.
+			// method 1 - pass the analytics id from javascript
+			//gaAccountID : 'UA-1352431-7'
+
+			// method 2 - get the analytics id from the web page
+			// assumes it is being written to the page by other means (serverside code?)
+			gaAccountID : "'" + gaAccountId + "'"
+
+
+			// method 3 - get the analytics id from the web page, don't embed the GA code for us
+			// assumes the GA code AND the account Id have been placed on the html page
+//			gaAccountID : "'" + gaAccountId + "'",
+//			embedGACode : false
+
+
+
+
 		});
 	}
 
